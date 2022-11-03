238814db57d1fe6453db4118329755824a023870
Abdulkadir Badamasi <mahuta2012@yahoo.com>
2022-11-03 19:43:25 +0000

    error fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,12 @@
-
-dule for Base class
+#!/usr/bin/python3
+"""Module for Base class
 Contains the Base class for the AirBnB clone console.
 """
 
 import uuid
 from datetime import datetime
 from models import storage
+
 
 
 class BaseModel:
